c8978843c2ee5b187e32ce23b6bc389b187d39bb
Borja Quevedo <borjota@gmail.com>
2021-06-21 00:14:22 +0200

    ✨ Show subway lines and stations

--- live_challenge_2/androidApp/build.gradle.kts
+++ live_challenge_2/androidApp/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    id("kotlin-android")
 }
 
 dependencies {
@@ -10,6 +11,11 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.2.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha02")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+
+    implementation("androidx.compose.ui:ui:1.0.0-beta09")
+    implementation("androidx.compose.material:material:1.0.0-beta09")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-beta09")
+    implementation("androidx.activity:activity-compose:1.3.0-beta02")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-native-mt")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0-native-mt")
@@ -30,10 +36,27 @@ android {
         targetSdk = 30
         versionCode = 1
         versionName = "1.0"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.0-beta09"
+        kotlinCompilerVersion = "1.5.10"
     }
 }
--- live_challenge_2/androidApp/src/main/java/dev/niltsiar/kmptmbtest/android/MainActivity.kt
+++ live_challenge_2/androidApp/src/main/java/dev/niltsiar/kmptmbtest/android/MainActivity.kt
@@ -2,10 +2,35 @@ package dev.niltsiar.kmptmbtest.android
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.niltsiar.kmptmbtest.android.ui.theme.AppTheme
+import dev.niltsiar.kmptmbtest.remote.SubwayLineProperties
+import dev.niltsiar.kmptmbtest.remote.SubwayStationProperties
 import dev.niltsiar.kmptmbtest.remote.TmbApiClient
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
@@ -17,36 +42,138 @@ import io.ktor.client.features.logging.Logging
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 
+private val client = HttpClient(OkHttp) {
+    install(JsonFeature) {
+        serializer = KotlinxSerializer(Json {
+            ignoreUnknownKeys = true
+        })
+    }
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) {
+                Log.v("KTOR", message)
+            }
+        }
+        level = LogLevel.ALL
+    }
+}
+
+private val apiClient = TmbApiClient(client)
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val client = HttpClient(OkHttp) {
-            install(JsonFeature) {
-                serializer = KotlinxSerializer(Json {
-                    ignoreUnknownKeys = true
-                })
-            }
-            install(Logging) {
-                logger = object : Logger {
-                    override fun log(message: String) {
-                        Log.v("KTOR", message)
-                    }
-                }
-                level = LogLevel.ALL
+        setContent {
+            KmpTestApp {
+                ScreenContent()
             }
         }
+    }
+}
 
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                val apiClient = TmbApiClient(client)
-                val times = apiClient.getTimesFromBusStop(929)
+@Composable
+fun KmpTestApp(content: @Composable () -> Unit) {
+    AppTheme(darkTheme = false) {
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
+}
 
-                times.forEach {
-                    Log.d("TIMES", "$it")
+@Composable
+fun ScreenContent() {
+    val scope = rememberCoroutineScope()
+
+    Column(modifier = Modifier.fillMaxHeight()) {
+
+        val (lines, setLines) = remember { mutableStateOf(emptyList<SubwayLineProperties>()) }
+
+        scope.launch {
+            setLines(apiClient.getSubwayLines().map { it.properties })
+        }
+
+        SubwayLinesList(lines = lines, modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun SubwayLinesList(
+    modifier: Modifier = Modifier,
+    lines: List<SubwayLineProperties>
+) {
+    val (selectedLine, setSelectedLine) = remember { mutableStateOf(Int.MIN_VALUE) }
+
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp),
+    ) {
+        items(items = lines) { line ->
+            SubwayLineCell(
+                subwayLine = line,
+                onClick = setSelectedLine,
+                isSelected = selectedLine == line.lineCode
+            )
+        }
+    }
+}
+
+@Composable
+fun SubwayLineCell(
+    subwayLine: SubwayLineProperties,
+    onClick: (Int) -> Unit,
+    isSelected: Boolean = false
+) {
+    val scope = rememberCoroutineScope()
+    val (stations, setStations) = remember { mutableStateOf<List<SubwayStationProperties>>(emptyList()) }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(1.dp, MaterialTheme.colors.onSurface, RoundedCornerShape(4.dp))
+            .clickable { onClick(subwayLine.lineCode) },
+    ) {
+        if (isSelected) {
+            scope.launch {
+                setStations(apiClient.getStationFromSubwayLine(subwayLine.lineCode).map { it.properties })
+            }
+        }
+        Column {
+            Text(text = subwayLine.lineName, modifier = Modifier.padding(24.dp))
+            if (stations.isNotEmpty()) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    stations.sortedBy { it.stationOrder }
+                        .forEach {
+                            Surface(
+                                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp),
+                                elevation = 8.dp,
+                                shape = RoundedCornerShape(16.dp),
+                                border = BorderStroke(
+                                    width = 1.dp,
+                                    color = MaterialTheme.colors.secondary
+                                )
+                            ) {
+                                Text(
+                                    text = it.stationName,
+                                    modifier = Modifier.padding(8.dp)
+                                )
+                            }
+                        }
                 }
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SubwayLineCellPreview() {
+    SubwayLineCell(
+        subwayLine = SubwayLineProperties(1, "L4"),
+        onClick = {}
+    )
 }
