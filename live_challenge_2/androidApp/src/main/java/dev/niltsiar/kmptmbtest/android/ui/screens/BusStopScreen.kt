d319cae9e5f4567c6479b33fbbaaeb9948584a34
Borja Quevedo <borjota@gmail.com>
2021-06-23 14:47:14 +0200

    ✨ Add custom view for marker information

--- live_challenge_2/androidApp/src/main/java/dev/niltsiar/kmptmbtest/android/ui/screens/BusStopScreen.kt
+++ live_challenge_2/androidApp/src/main/java/dev/niltsiar/kmptmbtest/android/ui/screens/BusStopScreen.kt
@@ -1,5 +1,12 @@
 package dev.niltsiar.kmptmbtest.android.ui.screens
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -16,15 +23,18 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.ktx.addMarker
 import com.google.maps.android.ktx.awaitMap
+import dev.niltsiar.kmptmbtest.android.R
 import dev.niltsiar.kmptmbtest.android.apiClient
 import dev.niltsiar.kmptmbtest.android.ui.rememberMapViewWithLifecycle
 import dev.niltsiar.kmptmbtest.android.ui.setZoom
@@ -37,18 +47,18 @@ import kotlinx.coroutines.launch
 @Composable
 fun BusStopMapBody() {
     val scope = rememberCoroutineScope()
-    var lines by remember { mutableStateOf(emptyList<Feature<BusStopProperties>>()) }
+    var busStops by remember { mutableStateOf(emptyList<Feature<BusStopProperties>>()) }
     var selectedBusStop by remember { mutableStateOf<BusStopInfo?>(null) }
 
     Surface(color = MaterialTheme.colors.background) {
 
         scope.launch {
-            lines = apiClient.getBusStops()
+            busStops = apiClient.getBusStops()
         }
 
         val onSelectedLine: (Int) -> Unit = { stopCode ->
             scope.launch {
-                val busStopProperties = lines.firstOrNull { it.properties.stopCode == stopCode } ?: return@launch
+                val busStopProperties = busStops.firstOrNull { it.properties.stopCode == stopCode } ?: return@launch
                 val times = apiClient.getTimesFromBusStop(stopCode)
                 selectedBusStop = BusStopInfo(busStopProperties, times)
             }
@@ -61,7 +71,7 @@ fun BusStopMapBody() {
         ) {
             ExpandedDropDownMenu(
                 label = "Bus stops",
-                items = lines.sortedBy { it.properties.stopName }
+                items = busStops.sortedBy { it.properties.stopName }
                     .map { it.properties.stopCode to it.properties.stopName },
                 onClick = onSelectedLine
             )
@@ -88,6 +98,7 @@ private fun MapView(initialPosition: LatLng, selectedBusStop: BusStopInfo?) {
     MapViewContainer(mapView, initialPosition, selectedBusStop)
 }
 
+@SuppressLint("PotentialBehaviorOverride")
 @Composable
 private fun MapViewContainer(
     map: MapView,
@@ -108,22 +119,57 @@ private fun MapViewContainer(
     var currentMarker: Marker? by remember { mutableStateOf(null) }
 
     val coroutineScope = rememberCoroutineScope()
-    AndroidView({ map }) { mapView ->
 
+    val layoutInflater = LayoutInflater.from(LocalContext.current)
+
+    AndroidView({ map }) { mapView ->
         // Reading zoom so that AndroidView recomposes when it changes. The getMapAsync lambda
         // is stored for later, Compose doesn't recognize state reads
         val mapZoom = zoom
         coroutineScope.launch {
             val googleMap = mapView.awaitMap()
             googleMap.setZoom(mapZoom)
+
+            googleMap.setInfoWindowAdapter(object : GoogleMap.InfoWindowAdapter {
+
+                val snippetView = layoutInflater.inflate(R.layout.custom_info_contents, null)
+
+                override fun getInfoWindow(p0: Marker): View? = null
+
+                override fun getInfoContents(marker: Marker): View? {
+                    val titleUi: TextView = snippetView.findViewById(R.id.title)
+                    val snippetUi: TextView = snippetView.findViewById(R.id.snippet)
+
+                    val title: String? = marker.title
+                    if (title != null) {
+                        // Spannable string allows us to edit the formatting of the text.
+                        titleUi.text = SpannableString(title).apply {
+                            setSpan(ForegroundColorSpan(Color.RED), 0, length, 0)
+                        }
+                    } else {
+                        titleUi.text = ""
+                    }
+
+                    val snippet: String? = marker.snippet
+                    if (snippet != null && snippet.length > 12) {
+                        snippetUi.text = SpannableString(snippet).apply {
+                            setSpan(ForegroundColorSpan(Color.BLUE), 0, length, 0)
+                        }
+                    } else {
+                        snippetUi.text = ""
+                    }
+
+                    return snippetView
+                }
+            })
             // Move camera to the same place to trigger the zoom update
             selectedBusStop?.let { busStopInfo ->
                 currentMarker?.remove()
                 val point = busStopInfo.busStopProperties.geometry as? Point ?: return@let
                 val latLng = LatLng(point.coordinates.latitude, point.coordinates.longitude)
                 val title =
-                    "${busStopInfo.busStopProperties.properties.stopCode} - ${busStopInfo.busStopProperties.properties.stopName}\n"
-                val times = busStopInfo.busStopTimes.map { "Line ${it.line} / Wait for ${it.remainingTime} minutes" }
+                    "Stop ${busStopInfo.busStopProperties.properties.stopCode} - ${busStopInfo.busStopProperties.properties.stopName}\n"
+                val times = busStopInfo.busStopTimes.map { "Line ${it.line} / Next bus in ${it.remainingTime} minutes" }
                     .ifEmpty { listOf("No information about waiting times") }
                 currentMarker = googleMap.addMarker {
                     position(latLng)
